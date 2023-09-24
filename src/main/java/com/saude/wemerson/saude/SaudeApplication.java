19a7d68598299c90fd7df23d3af67e68e69abfa5
Wemerson Nino <wemersonnino@gmail.com>
2023-09-24 20:22:30 -0300

    Configurando o projeto spring e criando os htmls

---
+++ src/main/java/com/saude/wemerson/saude/SaudeApplication.java
@@ -0,0 +1,13 @@
+package com.saude.wemerson.saude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaudeApplication.class, args);
+    }
+
+}
