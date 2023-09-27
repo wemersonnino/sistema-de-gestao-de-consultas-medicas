0311b3048a26433f06f8091183f0c98291164d92
Wemerson Nino <wemersonnino@gmail.com>
2023-09-27 15:48:27 -0300

    Separação do Header, Footer e criação do controller que chama o index.

---
+++ src/main/java/com/saude/wemerson/saude/controllers/MainController.java
@@ -0,0 +1,12 @@
+package com.saude.wemerson.saude.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String getIndex(){
+        return "index";
+    }
+}
